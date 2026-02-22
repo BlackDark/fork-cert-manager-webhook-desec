cd7de6ad82a0241c20100598e2655c8f809ad5d4
Georg Rollinger-Tertulino <georg.rollinger@posteo.net>
2026-02-22 22:34:53 +0100

    test: randomize domain used in tests
    To allow running tests multiple times without interference.

--- main_test.go
+++ main_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	acmetest "github.com/cert-manager/cert-manager/test/acme"
 )
@@ -17,10 +22,23 @@ func TestRunsSuite(t *testing.T) {
 	// ChallengeRequest passed as part of the test cases.
 	//
 
+	// Generate a random DNS challenge key for testing
+	randomBytes := make([]byte, 32)
+	if _, err := rand.Read(randomBytes); err != nil {
+		t.Fatalf("Failed to generate random challenge key: %v", err)
+	}
+	randomKey := base64.RawURLEncoding.EncodeToString(randomBytes)
+
+	// Generate a random DNS name suffix for testing (hex encoded to ensure DNS-safe characters)
+	randomSuffix := hex.EncodeToString(randomBytes[:8])
+
 	fixture := acmetest.NewFixture(&deSECDNSProviderSolver{},
 		acmetest.SetResolvedZone(zone),
 		acmetest.SetAllowAmbientCredentials(false),
 		acmetest.SetManifestPath("testdata/desec"),
+		acmetest.SetDNSChallengeKey(randomKey),
+		acmetest.SetResolvedFQDN(fmt.Sprintf("cert-manager-dns01-tests-%s.%s", randomSuffix, zone)),
+		acmetest.SetPropagationLimit(time.Minute*15),
 	)
 
 	// need to uncomment and RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
